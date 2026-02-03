b83cd9e369886cf7227b03e316ada96408c2f68e
dennissheppard <dennis.sheppard@gmail.com>
2026-02-02 22:44:53 -0600

    next arrow bigger

--- client/src/components/ImageNav.tsx
+++ client/src/components/ImageNav.tsx
@@ -1,3 +1,4 @@
+import { useState, useEffect } from 'react';
 import styles from './ImageNav.module.css';
 
 interface Props {
@@ -5,8 +6,28 @@ interface Props {
 }
 
 function ImageNav({ onNext }: Props) {
+  const [isVisible, setIsVisible] = useState(false);
+
+  useEffect(() => {
+    const handleMouseMove = (e: MouseEvent) => {
+      const width = window.innerWidth;
+      
+      // Show if mouse is in the right 25% of the screen
+      const isRightZone = e.clientX > width * 0.75;
+
+      if (isRightZone) {
+        setIsVisible(true);
+      } else {
+        setIsVisible(false);
+      }
+    };
+
+    window.addEventListener('mousemove', handleMouseMove);
+    return () => window.removeEventListener('mousemove', handleMouseMove);
+  }, []);
+
   return (
-    <div className={styles.hoverZone}>
+    <div className={`${styles.container} ${isVisible ? styles.visible : ''}`}>
       <button
         className={styles.navButton}
         onClick={onNext}
